44a0c5a039b36751f77d015682297ac36b1f2d50
Johnny Sullivan <inquiries@john-sullivan.com>
2026-04-08 12:56:06 -0400

    Adding pdf

---
+++ CosmicConcord.java
@@ -0,0 +1,11 @@
+/*  John Sullivan
+    Cosmic Concordance
+    COP3503 Computer Science 2
+    CosmicConcord.java
+*/
+
+public class CosmicConcord {
+    public int solve() {
+        return 1;
+    }
+}
