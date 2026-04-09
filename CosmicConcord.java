c444572702bc47f2042b75b556041f5983cc15e2
John Sullivan <sullj105@gmail.com>
2026-04-08 20:56:59 -0400

    Memoization done, need to do Tabulation next

--- CosmicConcord.java
+++ CosmicConcord.java
@@ -6,33 +6,66 @@
 
 public class CosmicConcord {
 	protected static int[] A, B;
-	protected static int D, G, K;
+	protected static int D, G, K, N, M;
 
     public static void solve(int[] a, int[] b, int n, int m, int d, int g, int k) {
-        A = a; B = b; D = d; G = g; K = k;
-    	// call solveRecMemo and solveTab here, print results
+        A = a; B = b; D = d; G = g; K = k; N = n; M = m;
+		int[][][] memo = new int[N][M][K+1]; // O(N * M * K)
+		for (int i = 0; i < N; i++) 
+            for (int j = 0; j < M; j++)
+				for (int h = 0; h < K + 1; h++)
+                	memo[i][j][h] = -1; 
+
+    	// call solveRecMemo and solveTab here then print results via schema
+		// memoization
+		/*
+		- need a startup loop to find first best pair
+		- start up loop will be O(N * M), with subsequent calls potentially exploring O(N * M),
+		  with those calls exploring O(K) dip levels
+		- therefore O(N^2 * M^2 * K)
+		*/
+		int memoLen = -1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (Math.abs(A[i] - B[j]) <= D)
+					memoLen = Math.max(memoLen, solveRecMemo(i, j, memo, 0 ));
+			}
+		}
+
+		// tabulation
+		int tabLen = solveTab(N, M);
+
+		// print
     }
 
-    public int solveRecMemo(int N, int M, int[][] memo) {
-        if (N == 0 || M == 0) 
-            return 0; 
-  
-        if (memo[N][M] != -1) 
-            return memo[N][M]; 
-  
-        if (A[N - 1] == B[M - 1]) 
-		{ 
-            memo[N][M] = 1 + solveRecMemo(N - 1, M - 1, memo); 
-            return memo[N][M]; 
-        } 
-  
-        memo[N][M] = Math.max(solveRecMemo(N, M - 1, memo), solveRecMemo(N - 1, M, memo)); 
-        return memo[N][M]; 
+    public static int solveRecMemo(int i, int j, int[][][] memo, int dips) {
+        if (memo[i][j][dips] != -1) 
+        	return memo[i][j][dips];
+
+		int length = 0;
+		for (int ni = i + 1; ni < N; ni++)
+		{
+			for (int nj = j + 1; nj < M; nj++)
+			{
+				if (Math.abs(A[ni] - B[nj]) <= D)
+				{ 
+					// increase check by growth value
+					boolean inc = (A[ni] - A[i] >= G) && (B[nj] - B[j] >= G);
+					if (inc) // if it is increasing, dont increase dip count
+            			length = Math.max(length, solveRecMemo(ni, nj, memo, dips));
+					else if (dips + 1 <= K) // increase dip count if it doesnt fail <= K
+						length = Math.max(length, solveRecMemo(ni, nj, memo, dips + 1));
+        		} 
+			}
+		}
+        
+		// add 1 (current match) plus best extension we have so far
+        memo[i][j][dips] = length + 1;
+        return memo[i][j][dips]; 
     }
 
-    public int solveTab(int N, int M, int[][] memo) {
+    public static int solveTab(int N, int M) {
         int c[][] = new int[N + 1][M + 1]; 
-		char b[][] = new char[N + 1][M + 1]; 
 		
 		for(int i = 0; i <= N; ++i)
 			c[i][0] = 0;
@@ -48,28 +81,17 @@ public class CosmicConcord {
                 if (A[i - 1] == B[j - 1]) 
 				{
                     c[i][j] = c[i - 1][j - 1] + 1; 
-					b[i][j] = 'd';
 				}
                 else if(c[i - 1][j] >= c[i][j - 1])
 				{
                     c[i][j] = c[i - 1][j];
-					b[i][j] = 'v';
 				}
 				else
 				{
 					c[i][j] = c[i][j - 1];
-					b[i][j] = 'h';
 				}
             } 
         }
         return c[N][M]; 
     }
-
-    public static int max(int a, int b) 
-	{ 
-		if(a > b)
-			return a;
-		else
-			return b;
-	} 
 }
